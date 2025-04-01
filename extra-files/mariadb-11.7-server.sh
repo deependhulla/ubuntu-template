3e4a4b74ea890e044c5fc85a6cb3f96a611fc295
Deepen Dhulla <deepen@deependhulla.com>
2025-04-01 17:46:45 +0530

    update on Tue Apr  1 05:46:45 PM IST 2025

--- extra-files/mariadb-11.7-server.sh
+++ extra-files/mariadb-11.7-server.sh
@@ -4,3 +4,24 @@
 curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
 echo "deb [signed-by=/etc/apt/keyrings/mariadb-keyring.pgp] https://atl.mirrors.knownhost.com/mariadb/repo/11.rolling/ubuntu noble main" >  /etc/apt/sources.list.d/mariadb.list
 
+apt-get update
+apt-get install mariadb-server
+
+sed -i "s/LimitNOFILE=32768/LimitNOFILE=62768/"   /usr/lib/systemd/system/mariadb.service
+systemctl daemon-reload
+
+MYSQLPASSVPOP=`pwgen -c -1 8`
+echo $MYSQLPASSVPOP > /usr/local/src/mariadb-mydbadmin-pass
+echo "mydbadmin password in /usr/local/src/mariadb-mydbadmin-pass"
+
+echo "GRANT ALL PRIVILEGES ON *.* TO mydbadmin@localhost IDENTIFIED BY '$MYSQLPASSVPOP'" with grant option | mariadb -uroot
+mariadb-admin -uroot reload
+mariadb-admin -uroot refresh
+
+systemctl restart  mariadb
+
+#mysqladmin -u root -ptmppassword status
+#mysqladmin -u root -p extended-status
+#mysqladmin -u root -p processlist
+
+echo "Database Password Setup done."
--- extra-files/phpmyadmin-and-apache2-setup.sh
+++ extra-files/phpmyadmin-and-apache2-setup.sh
@@ -6,5 +6,6 @@ echo "phpmyadmin phpmyadmin/app-password-confirm password" | sudo debconf-set-se
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password" | sudo debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password" | sudo debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y phpmyadmin libapache2-mod-php libapache2-mod-php8.3 libconfig-inifiles-perl  php-intl php-json php8.3-intl tnef php-pear php-apcu
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y phpmyadmin libapache2-mod-php libapache2-mod-php8.3 libconfig-inifiles-perl  php-intl php-json php8.3-intl tnef php-pear php-apcu php-phpdbg poppler-utils
 
+systemctl restart apache2
