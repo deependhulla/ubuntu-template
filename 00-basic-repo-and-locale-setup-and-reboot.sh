3d452a5f2a19f203e638aeff2288848ce5149b4f
Deepen Dhulla <deepen@deependhulla.com>
2025-04-01 14:21:50 +0530

    update on Tue Apr  1 02:21:50 PM IST 2025

--- 00-basic-repo-and-locale-setup-and-reboot.sh
+++ 00-basic-repo-and-locale-setup-and-reboot.sh
@@ -5,13 +5,13 @@ DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get -y full-upgrade
-apt -y install locales whiptail  
+apt -y install locales whiptail chrony 
 
-apt -y  install vim chrony jq conntrack openssh-server locales screen net-tools git mc tmux sendemail \
+apt -y  install vim jq conntrack openssh-server locales screen net-tools git mc tmux sendemail \
 sudo wget curl ethtool bridge-utils iptraf-ng traceroute telnet software-properties-common \
 dirmngr parted gdisk apt-transport-https whiptail rsyslog lsb-release iptables ca-certificates iputils-ping \
 debconf-utils gnupg pwgen xfsprogs nmap iftop htop multitail net-tools elinks pssh apache2 \
-socat ipset gnupg2 zip tar pv rar unrar rsync unzip vnstat ebtables openvswitch-switch 
+socat ipset gnupg2 zip tar pv rar unrar rsync unzip vnstat ebtables  
 
 ## set to India IST timezone -- You can dissable it if needed
 timedatectl set-timezone 'Asia/Kolkata'
