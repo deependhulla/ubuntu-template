14fa6e14a7a9d24ac72cef21b3661688cff055b3
Deepen Dhulla <deepen@deependhulla.com>
2025-08-04 14:20:15 +0530

    Update 00-basic-repo-and-locale-setup-and-reboot.sh
    added itop

--- 00-basic-repo-and-locale-setup-and-reboot.sh
+++ 00-basic-repo-and-locale-setup-and-reboot.sh
@@ -17,7 +17,7 @@ apt -y install vim jq postfix conntrack openssh-server locales screen net-tools 
 sudo wget curl ethtool bridge-utils iptraf-ng traceroute telnet arping software-properties-common \
 dirmngr parted gdisk apt-transport-https whiptail lsb-release iptables ca-certificates iputils-ping \
 debconf-utils gnupg pwgen xfsprogs nmap iftop htop multitail net-tools elinks pssh ubuntu-advantage-tools \
-socat ipset iptables-persistent gnupg2 zip tar pv auditd rar unrar rsync unzip vnstat ebtables  
+socat ipset iptables-persistent gnupg2 zip tar pv auditd rar itop unrar rsync unzip vnstat ebtables  
 
 ## set to India IST timezone -- You can dissable it if needed
 timedatectl set-timezone 'Asia/Kolkata'
